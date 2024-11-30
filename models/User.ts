659541bd3f4fcde7a63fc24fe5e8ec8083aa33c9
pawelek <k.pawelek@vp.pl>
2024-11-30 14:28:59 +0100

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -12,12 +12,21 @@ const UserSchema: Schema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-})
+},
+    { timestamps: true })
 
-const User = mongoose.model<IUserType>('User', UserSchema)
+const User = mongoose.models.User || mongoose.model<IUserType>('User', UserSchema);
 
-export const createUser = async (user: IUser) => {
-    const newUser = new User(user);
-    await newUser.save();
-    console.log(newUser._id);
-}
+export const createUser = async (user: IUser): Promise<IUserType | null> => {
+    try {
+        const newUser = new User(user);
+        await newUser.save();
+        console.log(`User created: ${newUser._id}`);
+        return newUser;
+    } catch (err) {
+        console.error(err.message);
+        throw new Error(err.message);
+    }
+};
+
+export default User;
