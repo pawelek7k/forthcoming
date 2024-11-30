bc4560bc595066dc98ef6aafa68718d32f8b0a1e
pawelek <k.pawelek@vp.pl>
2024-11-30 14:20:09 +0100

    Update Heading.tsx

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import React from "react";
 
 type HeadingType = {
-  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   children: ChildrenType;
   className?: ClassNameType;
 } & React.HTMLAttributes<HTMLHeadingElement>;
