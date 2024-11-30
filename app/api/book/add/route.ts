38613845971f191a39b6ef12cca26c878015863f
pawelek <k.pawelek@vp.pl>
2024-11-30 16:29:34 +0100

    .

---
+++ app/api/book/add/route.ts
@@ -0,0 +1,27 @@
+import { authOptions } from "@/lib/authOptions";
+import { connectToDatabase } from "@/lib/mongoDB/connect";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const session = await getServerSession(authOptions);
+
+        if (!session) {
+            return NextResponse.json(
+                { message: 'You need to be logged in to view books.' },
+                { status: 401 }
+            );
+        }
+
+        const db = await connectToDatabase();
+        const books = await db.collection('books').find({}).toArray();
+
+        return NextResponse.json(books, { status: 200 });
+    } catch (err) {
+        return NextResponse.json(
+            { error: 'Failed to fetch books' },
+            { status: 500 }
+        );
+    }
+}
--- lib/redux/slices/booksSlice.ts
+++ lib/redux/slices/booksSlice.ts
@@ -1,27 +1,27 @@
-// import type { Book } from "@/types/book";
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
+import type { Book } from "@/types/book";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-// type BooksStateType = {
-//     books: Book[];
-//     status: 'idle' | 'loading' | 'succeeded' | 'failed';
-//     error: string | null;
-// }
+type BooksStateType = {
+    books: Book[];
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    error: string | null;
+}
 
-// const initialState: BooksStateType = {
-//     books: [],
-//     status: 'idle',
-//     error: null
-// }
+const initialState: BooksStateType = {
+    books: [],
+    status: 'idle',
+    error: null
+}
 
-// export const fetchBooks = createAsyncThunk(
-//     'books/fetchBooks',
-//     async (_, { rejectWithValue }) => {
-//         try {
-//             const response = await axios.get('/api/books');
-//             return response.data;
-//         } catch (err) {
-//             return rejectWithValue(err.response?.data || 'Failed to fetch books');
-//         }
-//     }
-// );
+export const fetchBooks = createAsyncThunk(
+    'books/fetchBooks',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await axios.get('/api/book/get');
+            return response.data;
+        } catch (err) {
+            return rejectWithValue(err.response?.data || 'Failed to fetch books');
+        }
+    }
+);
