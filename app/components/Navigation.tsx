449955d61ae839cd9d7df39a693ceaaec937edde
pawelek <k.pawelek@vp.pl>
2024-12-01 18:37:55 +0100

    Update Navigation.tsx

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { Sling as Hamburger } from "hamburger-react";
+import { useState } from "react";
 import { LinkComponent as Link } from "./Link";
 import { Logo } from "./Logo";
 
@@ -7,6 +11,7 @@ type LinkType = {
 }[];
 
 export const Navigation = () => {
+  const [isOpen, setOpen] = useState(false);
   const links: LinkType = [
     { label: "Home", path: "/home" },
     { label: "Create", path: "/myworks/create" },
@@ -17,7 +22,7 @@ export const Navigation = () => {
   return (
     <header className="flex justify-around w-full fixed z-40 backdrop-blur-md rounded-b-lg top-0 items-center shadow-lg pl-12 gap-12">
       <Logo />
-      <div className="bg-zinc-100 rounded-l-full border border-rose-950 text-zinc-950 p-4 pl-12 w-full">
+      <div className="bg-zinc-100 rounded-l-full border border-rose-950 text-zinc-950 p-1 px-12 w-full flex justify-between">
         <nav className="flex items-center">
           <ul className="flex gap-4">
             {links.map(({ label, path }) => (
@@ -27,7 +32,14 @@ export const Navigation = () => {
             ))}
           </ul>
         </nav>
-        {/* hamburger */}
+        <div className="rounded-full bg-zinc-950">
+          <Hamburger
+            size={20}
+            toggled={isOpen}
+            toggle={setOpen}
+            color="#f4f4f5"
+          />
+        </div>
       </div>
     </header>
   );
