b994879ff08b1ad92ca1b01604db3ecb1a128418
pawelek <k.pawelek@vp.pl>
2024-12-01 19:38:21 +0100

    Create Cover.tsx

---
+++ app/components/Cover.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+type CoverProps = {
+  title: string;
+  cover: string;
+};
+
+export const Cover = ({ title, cover }: CoverProps) => {
+  return (
+    <div>
+      <div className="relative overflow-hidden rounded-md sm:w-36 sm:h-56 w-full h-40">
+        <Image
+          src={cover}
+          alt={title}
+          fill
+          style={{ objectFit: "cover", objectPosition: "center" }}
+          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+          loading="lazy"
+        />
+      </div>
+    </div>
+  );
+};
