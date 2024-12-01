ff6f2543c1c0f5fc3f211226098825497e083c09
pawelek <k.pawelek@vp.pl>
2024-12-01 19:39:20 +0100

    Update BookItem.tsx

--- app/components/BookItem.tsx
+++ app/components/BookItem.tsx
@@ -1,4 +1,5 @@
 import { Book } from "@/types/book";
+import { Cover } from "./Cover";
 
 export const BookItem = ({ book }: { book: Book }) => {
   return (
@@ -6,6 +7,7 @@ export const BookItem = ({ book }: { book: Book }) => {
       key={book._id.toString()}
       className="cursor-pointer dark:shadow-sm flex gap-2 p-2 transition ease-in-out rounded-lg hover:shadow-sm flex-col sm:flex-row bg-zinc-900 hover:bg-zinc-800"
     >
+      <Cover title={book.title} cover={book.cover} />
       <div className="flex flex-col sm:flex-row gap-2">
         <div className="p-2 flex flex-col gap-2">
           <p className="text-sm md:text-base">{book.title}</p>
