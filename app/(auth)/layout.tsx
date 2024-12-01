29f561cf19b4367f42662831f06f3f816a7e7e24
pawelek <k.pawelek@vp.pl>
2024-12-01 20:03:50 +0100

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <ReduxProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-950 text-zinc-100`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-primary-bg text-zinc-100`}
         >
           <NextAuthProvider>
             <Navigation />
