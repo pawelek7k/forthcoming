9d115ad46bd7c35c9dbf5bb1fad18a089497d4a5
pawelek <k.pawelek@vp.pl>
2024-12-01 19:36:35 +0100

    Update BookItem.tsx

--- app/components/BookItem.tsx
+++ app/components/BookItem.tsx
@@ -4,7 +4,7 @@ export const BookItem = ({ book }: { book: Book }) => {
   return (
     <li
       key={book._id.toString()}
-      className="cursor-pointer dark:shadow-sm flex gap-2 p-2 transition ease-in-out rounded-lg hover:shadow-sm flex-col sm:flex-row bg-zinc-900"
+      className="cursor-pointer dark:shadow-sm flex gap-2 p-2 transition ease-in-out rounded-lg hover:shadow-sm flex-col sm:flex-row bg-zinc-900 hover:bg-zinc-800"
     >
       <div className="flex flex-col sm:flex-row gap-2">
         <div className="p-2 flex flex-col gap-2">
