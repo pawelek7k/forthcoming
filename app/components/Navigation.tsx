3fd20e93caf69397af3dc4dec4a4753c9a9c1676
pawelek <k.pawelek@vp.pl>
2024-12-01 16:31:26 +0100

    Update Navigation.tsx

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -17,12 +17,12 @@ export const Navigation = () => {
   return (
     <header className="flex justify-around w-full fixed z-40 backdrop-blur-md rounded-b-lg top-0 items-center shadow-lg pl-12 gap-12">
       <Logo />
-      <div className="bg-zinc-950 rounded-l-full border border-rose-950 text-zinc-100 p-4 pl-12 w-full">
+      <div className="bg-zinc-100 rounded-l-full border border-rose-950 text-zinc-950 p-4 pl-12 w-full">
         <nav className="flex items-center">
           <ul className="flex gap-4">
             {links.map(({ label, path }) => (
-              <li key={label}>
-                <Link to={path}>{label}</Link>
+              <li key={label} className="font-semibold text-sm hover-effect">
+                <Link to={path}>{label.toUpperCase()}</Link>
               </li>
             ))}
           </ul>
