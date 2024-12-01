c8993c90fc2849ef9f6c5c3b61e8f662ed97fb56
pawelek <k.pawelek@vp.pl>
2024-12-01 16:22:26 +0100

    add logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,7 +1,12 @@
+import { IoIosMoon } from "react-icons/io";
 import { LinkComponent as Link } from "./Link";
 
 export const Logo = () => (
   <Link to="/home">
-    <div>Forthcoming</div>
+    <div className="flex items-center font-semibold">
+      <span className="text-rose-100">Forth</span>
+      <IoIosMoon />
+      <span>oming</span>
+    </div>
   </Link>
 );
