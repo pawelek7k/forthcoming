17427a6d2c8c5bc84a9af42ca4dcb9ee1c40a718
pawelek <k.pawelek@vp.pl>
2024-12-01 16:12:03 +0100

    Update Navigation.tsx

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -15,9 +15,9 @@ export const Navigation = () => {
   ];
 
   return (
-    <header className="flex justify-around w-full fixed z-40 backdrop-blur-md rounded-b-lg border-b top-0 items-center dark:border-b-rose-950 shadow-lg">
+    <header className="flex justify-around w-full fixed z-40 backdrop-blur-md rounded-b-lg top-0 items-center shadow-lg pl-12 gap-12">
       <Logo />
-      <div className="bg-zinc-100/90 rounded-l-full text-zinc-900 p-4">
+      <div className="bg-zinc-950 rounded-l-full border border-rose-950 text-zinc-100 p-4 pl-12 w-full">
         <nav className="flex items-center">
           <ul className="flex gap-4">
             {links.map(({ label, path }) => (
