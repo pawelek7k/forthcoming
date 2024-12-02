571b0ec40d865207306dc7a7a58bee42c3675e59
pawelek <k.pawelek@vp.pl>
2024-12-02 20:20:11 +0100

    Update layout.tsx

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import localFont from "next/font/local";
 import { Navigation } from "../../components/Navigation";
 import "../../globals.css";
@@ -15,21 +17,26 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  const messages = await getMessages();
   return (
     <ReduxProvider>
-      <html lang="en">
+      <html lang={locale || "en"}>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-primary-bg text-zinc-100`}
         >
-          <NextAuthProvider>
-            <Navigation />
-            <main className="pt-20 p-6 min-h-screen">{children}</main>
-          </NextAuthProvider>
+          <NextIntlClientProvider messages={messages}>
+            <NextAuthProvider>
+              <Navigation />
+              <main className="pt-20 p-6 min-h-screen">{children}</main>
+            </NextAuthProvider>
+          </NextIntlClientProvider>
         </body>
       </html>
     </ReduxProvider>
