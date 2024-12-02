cc3b343005b7f7d47b8d554200e7b7a7993d79b7
pawelek <k.pawelek@vp.pl>
2024-12-02 10:23:53 +0100

    Update route.ts

--- app/api/book/add/route.ts
+++ app/api/book/add/route.ts
@@ -1,26 +1,42 @@
-
 import { ArrayGenres } from '@/lib/data/bookGenre';
 import { connectToDatabase } from '@/lib/mongoDB/connect';
 import { schema } from '@/models/book';
 
 import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/authOptions';
 
 export const validGenres = ArrayGenres.map(genre => genre.name);
 export const validLanguages = ['pl', 'eng'];
 
 export async function POST(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
     const body = await req.json();
     const { error, value } = schema.validate(body);
+
     if (error) {
         return NextResponse.json({ message: error.details[0].message }, { status: 400 });
     }
-    const data = value;
+
+    const data = {
+        ...value,
+        author: session.user?.email || 'Unknown',
+    };
 
     try {
         const db = await connectToDatabase();
         const booksCollection = db.collection('books');
         const result = await booksCollection.insertOne(data);
-        return NextResponse.json({ message: 'Book created!', id: result.insertedId.toString() }, { status: 201 });
+
+        return NextResponse.json(
+            { message: 'Book created!', id: result.insertedId.toString() },
+            { status: 201 }
+        );
     } catch (err) {
         console.error(err);
         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
