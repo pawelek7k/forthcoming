ce01ec5f472c7f13dec5f8588ca25dc2e593eda3
pawelek <k.pawelek@vp.pl>
2024-12-03 17:12:17 +0100

    Update Navigation.tsx

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -20,27 +20,29 @@ export const Navigation = () => {
   ];
 
   return (
-    <header className="flex justify-around w-full fixed z-40 backdrop-blur-md rounded-b-lg top-0 items-center shadow-lg pl-12 gap-12">
-      <Logo />
-      <div className="bg-zinc-100 rounded-l-full border border-rose-950 text-zinc-950 p-1 px-12 w-full flex justify-between">
-        <nav className="flex items-center">
-          <ul className="flex gap-4">
-            {links.map(({ label, path }) => (
-              <li key={label} className="font-semibold text-sm hover-effect">
-                <Link to={path}>{label.toUpperCase()}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="rounded-full bg-zinc-950">
-          <Hamburger
-            size={20}
-            toggled={isOpen}
-            toggle={setOpen}
-            color="#f4f4f5"
-          />
+    <>
+      <header className="flex justify-around w-full fixed z-40 backdrop-blur-md rounded-b-lg top-0 items-center shadow-lg pl-12 gap-12">
+        <Logo />
+        <div className="bg-zinc-100 rounded-l-full border border-rose-950 text-zinc-950 p-1 px-12 w-full flex justify-between">
+          <nav className="flex items-center">
+            <ul className="flex gap-4">
+              {links.map(({ label, path }) => (
+                <li key={label} className="font-semibold text-sm hover-effect">
+                  <Link to={path}>{label.toUpperCase()}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="rounded-full bg-zinc-950">
+            <Hamburger
+              size={20}
+              toggled={isOpen}
+              toggle={setOpen}
+              color="#f4f4f5"
+            />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
