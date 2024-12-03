390671bb649bcdbba9496966d987dac8edb7b925
pawelek <k.pawelek@vp.pl>
2024-12-03 17:52:50 +0100

    Update Filter.tsx

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { Button } from "./Button";
 import { Heading } from "./Heading";
 import { InputField } from "./InputField";
+import { ToggleSwitch } from "./ToggleSwitch";
 
 export const Filter = () => {
   return (
@@ -21,6 +22,16 @@ export const Filter = () => {
         placeholder={"Enter title"}
         label={"Enter title"}
       />
+      <div className="flex items-center gap-2 mb-4">
+        <span className="dark:text-neutral-100 text-sm font-medium text-gray-700">
+          For adult
+        </span>
+        <ToggleSwitch
+          name="forAdult"
+          //   value={filters.forAdult ? "on" : "off"}
+          //   onChange={(value) => handleInputChange("forAdult", value === "on")}
+        />
+      </div>
       <Button primary={true}>Submit</Button>
     </motion.div>
   );
