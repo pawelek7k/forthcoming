5a73d0630baa92233cc43651658f3233a1c2bb6e
pawelek <k.pawelek@vp.pl>
2024-12-04 18:31:22 +0100

    Create ModalBtns.tsx

---
+++ app/components/ModalBtns.tsx
@@ -0,0 +1,81 @@
+import { useRouter } from "@/navigation";
+import { Book } from "@/types/book";
+import { useSession } from "next-auth/react";
+import { useTranslations } from "next-intl";
+import Notiflix from "notiflix";
+import { useState } from "react";
+import { Loader } from "./Loader";
+import { Button } from "./Button";
+
+interface ButtonsProps {
+  book: Book;
+}
+
+export const Buttons: React.FC<ButtonsProps> = ({ book }) => {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const { data: session } = useSession();
+  const router = useRouter();
+  const t = useTranslations("global");
+
+  const handleAddToLibrary = async () => {
+    if (!session?.user) {
+      Notiflix.Notify.warning(
+        "You need to be logged in to add a book to your library."
+      );
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/user/library/add", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ bookId: book._id }),
+      });
+
+      if (response.ok) {
+        Notiflix.Notify.success("Book added to library!");
+      } else if (response.status === 409) {
+        Notiflix.Notify.warning("This book is already in your library.");
+      } else {
+        Notiflix.Notify.failure("Failed to add book to library.");
+      }
+    } catch (err) {
+      Notiflix.Notify.failure("Error adding book to library.", err);
+    }
+  };
+
+  const handleStartReading = () => {
+    try {
+      setIsSubmitting(true);
+      router.push(`/${book._id}`);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      {isSubmitting && <Loader />}
+      <ul className="flex justify-between mt-2 flex-col gap-4 sm:flex-row">
+        <li>
+          <Button
+            primary={true}
+            onClick={handleStartReading}
+            isSubmitting={isSubmitting}
+          >
+            {t("startReading")}
+          </Button>
+        </li>
+        <li>
+          <Button primary={true} onClick={handleAddToLibrary}>
+            {t("addToLibrary")}
+          </Button>
+        </li>
+      </ul>
+    </>
+  );
+};
