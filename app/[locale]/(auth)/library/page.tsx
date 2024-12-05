0a4dd62962bbdfafcb718cb6c6808166aa257786
pawelek <k.pawelek@vp.pl>
2024-12-05 16:48:27 +0100

    Create page.tsx

---
+++ app/[locale]/(auth)/library/page.tsx
@@ -0,0 +1,31 @@
+import { Heading } from "@/app/components/Heading";
+import { Loader } from "@/app/components/Loader";
+import { RenderBooks } from "@/app/components/RenderBooks";
+import { getUserLibrary } from "@/lib/mongoDB/getUserLibrary";
+import { redirect } from "@/navigation";
+import { Suspense } from "react";
+
+export const metadata = {
+  title: "Future - Your Library",
+  description: "Future",
+};
+
+const LibraryPage: React.FC = async () => {
+  const { books, userLibrary } = await getUserLibrary();
+
+  if (books.length === 0) {
+    redirect("/");
+    return null;
+  }
+
+  return (
+    <section>
+      <Heading as="h1">Your Library!</Heading>
+      <Suspense fallback={<Loader />}>
+        <RenderBooks books={books} userLibrary={userLibrary} />
+      </Suspense>
+    </section>
+  );
+};
+
+export default LibraryPage;
