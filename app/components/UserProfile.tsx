a3a293bb99638a3271f42381a3cfa907ad1b3aac
pawelek <k.pawelek@vp.pl>
2024-12-08 11:29:13 +0100

    Update UserProfile.tsx

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -6,10 +6,10 @@ export const UserProfile = () => {
   const email = session?.user?.email ?? "unknown@example.com";
   const username = session?.user?.name ?? email.split("@")[0];
   return (
-    <div className="flex items-center">
+    <div className="flex items-center gap-4">
       <div className="bg-zinc-100 w-8 h-8 rounded-full"></div>
-      <div className="flex flex-col">
-        <p>{email} </p>
+      <div className="flex flex-col text-sm">
+        <p>{email}</p>
         <p>{username}</p>
       </div>
     </div>
