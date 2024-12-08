ef6b6ed9bfb3023ad9f24a9780d776c00b673f10
pawelek <k.pawelek@vp.pl>
2024-12-08 11:24:20 +0100

    Update Welcome.tsx

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -6,7 +6,7 @@ type UserType = {
   username: string;
 };
 
-const Welcome: React.FC<UserType> = ({ email, username }) => {
+const Welcome = ({ email, username }: UserType) => {
   const t = useTranslations("headings");
   return (
     <div className="bg-black rounded-xl p-10 my-8 bg-home-img bg-top dark:bg-center dark:bg-dark-home-img bg-no-repeat bg-cover ">
