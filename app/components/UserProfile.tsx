8b9030a6e3fde5461e196d282e3f4974b07a3cc0
pawelek <k.pawelek@vp.pl>
2024-12-08 11:27:06 +0100

    Update UserProfile.tsx

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -1,3 +1,17 @@
+import { useSession } from "next-auth/react";
+
 export const UserProfile = () => {
-  return <div></div>;
+  const { data: session } = useSession();
+
+  const email = session?.user?.email ?? "unknown@example.com";
+  const username = session?.user?.name ?? email.split("@")[0];
+  return (
+    <div className="flex items-center">
+      <div className="bg-zinc-100 w-8 h-8 rounded-full"></div>
+      <div className="flex flex-col">
+        <p>{email} </p>
+        <p>{username}</p>
+      </div>
+    </div>
+  );
 };
