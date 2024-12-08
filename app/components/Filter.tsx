c52b954a2df8a5518aef86be0b34cfd006c6a045
pawelek <k.pawelek@vp.pl>
2024-12-08 11:28:43 +0100

    Update Filter.tsx

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -13,6 +13,7 @@ import { RootState } from "@/lib/redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { DropdownMenu } from "./Dropdown";
 import { ToggleSwitch } from "./ToggleSwitch";
+import { UserProfile } from "./UserProfile";
 
 export const Filter = () => {
   const dispatch = useDispatch();
@@ -41,54 +42,57 @@ export const Filter = () => {
 
   return (
     <motion.div
-      className="h-screen w-full bg-zinc-950 pt-20 flex flex-col p-6"
+      className="h-screen w-full bg-zinc-950 pt-20 p-6 flex-col flex justify-between"
       initial={{ opacity: 0, x: 20 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: 20 }}
       transition={{ duration: 0.3, ease: "easeInOut" }}
     >
-      <Heading as="h2" className="text-2xl font-semibold text-zinc-100 mb-4">
-        Filter
-      </Heading>
-      <InputField
-        id="filter-title"
-        onChange={handleInputChange}
-        placeholder="Enter title"
-        value={searchQuery}
-        label="Enter title"
-      />
-      <div className="flex justify-between flex-col gap-4">
-        <DropdownMenu onChange={handleGenreChange} />
-        <div className="flex gap-2 items-center justify-between">
-          <Heading as="h4">Language of books</Heading>
-          <div className="flex items-center gap-2">
-            <span className="text-sm">PL</span>
-            <ToggleSwitch
-              name="languageToggle"
-              value={selectedLanguage || "eng"}
-              onValue="pl"
-              offValue="eng"
-              onChange={(value) => dispatch(setSelectedLanguage(value))}
-            />
-            <span className="text-sm">ENG</span>
+      <div>
+        <Heading as="h2" className="text-2xl font-semibold text-zinc-100 mb-4">
+          Filter
+        </Heading>
+        <InputField
+          id="filter-title"
+          onChange={handleInputChange}
+          placeholder="Enter title"
+          value={searchQuery}
+          label="Enter title"
+        />
+        <div className="flex justify-between flex-col gap-4">
+          <DropdownMenu onChange={handleGenreChange} />
+          <div className="flex gap-2 items-center justify-between">
+            <Heading as="h4">Language of books</Heading>
+            <div className="flex items-center gap-2">
+              <span className="text-sm">PL</span>
+              <ToggleSwitch
+                name="languageToggle"
+                value={selectedLanguage || "eng"}
+                onValue="pl"
+                offValue="eng"
+                onChange={(value) => dispatch(setSelectedLanguage(value))}
+              />
+              <span className="text-sm">ENG</span>
+            </div>
+          </div>
+          <div className="flex gap-2 items-center justify-between">
+            <Heading as="h4">For Adult</Heading>
+            <div className="flex items-center gap-2 mb-4">
+              <span className="text-sm">NO</span>
+              <ToggleSwitch
+                name="forAdultToggle"
+                value={forAdult ? "on" : "off"}
+                onChange={() => dispatch(toggleForAdult())}
+              />
+              <span className="text-sm">YES</span>
+            </div>
           </div>
         </div>
-        <div className="flex gap-2 items-center justify-between">
-          <Heading as="h4">For Adult</Heading>
-          <div className="flex items-center gap-2 mb-4">
-            <span className="text-sm">NO</span>
-            <ToggleSwitch
-              name="forAdultToggle"
-              value={forAdult ? "on" : "off"}
-              onChange={() => dispatch(toggleForAdult())}
-            />
-            <span className="text-sm">YES</span>
-          </div>
-        </div>
+        <Button onClick={clearFilters} primary={true}>
+          Clear Filters
+        </Button>
       </div>
-      <Button onClick={clearFilters} primary={true}>
-        Clear Filters
-      </Button>
+      <UserProfile />
     </motion.div>
   );
 };
