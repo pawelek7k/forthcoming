521dcc880a40613f071e3418b6cf2e8d2b0e933c
pawelek <k.pawelek@vp.pl>
2024-12-08 13:24:27 +0100

    add details

--- app/[locale]/(auth)/[bookId]/page.tsx
+++ app/[locale]/(auth)/[bookId]/page.tsx
@@ -1,7 +1,57 @@
+import { Heading } from "@/app/components/Heading";
 import { Section } from "@/app/components/Section";
+import { getBookDetails } from "@/lib/mongoDB/getDetails";
+import Image from "next/legacy/image";
 
-const DetailsDynamicPage = () => {
-  return <Section>hej</Section>;
+type DetailsType = {
+  params: { bookId: string };
+};
+
+const DetailsDynamicPage = async ({ params }: DetailsType) => {
+  const { bookId } = params;
+
+  const book = await getBookDetails(bookId);
+
+  if (!book) return <section>Book not found</section>;
+
+  return (
+    <Section>
+      <div className="flex flex-col items-center  shadow-lg rounded-lg">
+        <div className="flex md:p-10 p-4 gap-12 flex-col md:flex-row items-center md:items-start">
+          <div>
+            <div className="relative overflow-hidden rounded-md md:w-48 md:h-80 w-40 h-60">
+              <Image
+                src={book.cover}
+                alt={book.title}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 ">
+            <Heading as="h1">{book.title}</Heading>
+            <p className="text-sm sm:text-base">{book.description}</p>
+            <div className="flex gap-10">
+              <Heading as="h3">For Adult:</Heading>{" "}
+              {book.forAdult ? "Yes" : "No"}
+              <Heading as="h3">Language of the book:</Heading>{" "}
+              {book.lang ? "PL" : "ENG"}
+            </div>
+            <Heading as="h3">Genre: </Heading>
+            {book.genre}
+          </div>
+        </div>
+      </div>
+      <div className="flex md:gap-20 flex-col md:flex-row">
+        {book.content && (
+          <div>
+            <div dangerouslySetInnerHTML={{ __html: book.content }} />
+          </div>
+        )}
+      </div>
+    </Section>
+  );
 };
 
 export default DetailsDynamicPage;
---
+++ lib/mongoDB/getDetails.ts
@@ -0,0 +1,30 @@
+import { Book } from '@/types/book';
+import { ObjectId } from 'mongodb';
+import { connectToDatabase } from './connect';
+
+export async function getBookDetails(bookId: string): Promise<Book | null> {
+    try {
+        const db = await connectToDatabase();
+
+        const booksCollection = db.collection('books');
+
+        const book = await booksCollection.findOne({ _id: new ObjectId(bookId) });
+
+        if (!book) return null;
+
+        return {
+            _id: book._id.toString(),
+            title: book.title,
+            cover: book.cover,
+            description: book.description,
+            forAdult: book.forAdult,
+            genre: book.genre,
+            tags: book.tags,
+            lang: book.lang,
+            content: book.content,
+        };
+    } catch (error) {
+        console.error('Error fetching book details:', error);
+        throw new Error('Failed to fetch book details');
+    }
+}
