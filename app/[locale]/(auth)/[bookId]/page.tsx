be1fd08f58bdd36972faf302cf0d24e96268f56a
pawelek <k.pawelek@vp.pl>
2024-12-09 18:47:49 +0100

    Update page.tsx

--- app/[locale]/(auth)/[bookId]/page.tsx
+++ app/[locale]/(auth)/[bookId]/page.tsx
@@ -4,6 +4,11 @@ import { Section } from "@/app/components/Section";
 import { getBookDetails } from "@/lib/mongoDB/getDetails";
 import Image from "next/legacy/image";
 
+export const metadata = {
+  title: "Future - The book you like",
+  description: "Read the books you like with your favorite wine and music.",
+};
+
 type DetailsType = {
   params: { bookId: string };
 };
