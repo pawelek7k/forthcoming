3a5e34cabd835bd2f1d390080c60a547b09d78dc
pawelek <k.pawelek@vp.pl>
2024-12-10 17:23:34 +0100

    add fixes

--- app/[locale]/(auth)/home/page.tsx
+++ app/[locale]/(auth)/home/page.tsx
@@ -3,13 +3,12 @@
 import { Loader } from "@/app/components/Loader";
 import { RenderBooks } from "@/app/components/RenderBooks";
 import { Section } from "@/app/components/Section";
+import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
 import { fetchBooks } from "@/lib/redux/slices/booksSlice";
-import { RootState } from "@/lib/redux/store";
 import { useRouter } from "@/navigation";
 import { useSession } from "next-auth/react";
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 const Welcome = dynamic(() => import("@/app/components/Welcome"), {
   ssr: false,
@@ -19,10 +18,8 @@ const Welcome = dynamic(() => import("@/app/components/Welcome"), {
 const HomePage = () => {
   const { data: session, status: sessionStatus } = useSession();
   const router = useRouter();
-  const dispatch = useDispatch();
-  const { books, status, error } = useSelector(
-    (state: RootState) => state.books
-  );
+  const dispatch = useAppDispatch();
+  const { books, status, error } = useAppSelector((state) => state.books);
 
   useEffect(() => {
     if (sessionStatus === "loading") return;
