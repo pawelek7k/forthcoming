781d75e53e7cd8d7cd5a0c9f9c2dc38e314a0aed
pawelek <k.pawelek@vp.pl>
2024-12-11 16:29:33 +0100

    Update EditorContainer.tsx

--- app/components/EditorContainer.tsx
+++ app/components/EditorContainer.tsx
@@ -12,6 +12,9 @@ type CreateChaptersType = {
 
 export const EditorContainer = ({ bookId }: CreateChaptersType) => {
   const [text, setText] = useState<string>("");
+  const [buttonState, setButtonState] = useState<
+    "idle" | "pending" | "fulfilled" | "rejected"
+  >("idle");
 
   if (!bookId) {
     console.error("RichTextEditor requires a valid bookId prop.");
@@ -21,16 +24,19 @@ export const EditorContainer = ({ bookId }: CreateChaptersType) => {
   const handleChange = (value: string) => setText(value);
 
   const handleSave = async () => {
+    setButtonState("pending");
     try {
       const response = await axios.put(`/api/book/update/${bookId}`, {
         content: text,
       });
       if (response.status === 200) {
+        setButtonState("fulfilled");
         Notiflix.Notify.success("Content saved successfully!");
       } else {
         throw new Error("Unexpected response status");
       }
     } catch (err) {
+      setButtonState("rejected");
       if (axios.isAxiosError(err)) {
         console.error(
           "Error saving content:",
@@ -41,14 +47,29 @@ export const EditorContainer = ({ bookId }: CreateChaptersType) => {
         console.error("Unexpected error:", err);
         Notiflix.Notify.warning("An unexpected error occurred");
       }
+    } finally {
+      setTimeout(() => setButtonState("idle"), 3000);
     }
   };
 
   return (
     <div className="w-full min-h-screen p-6 sm:p-10 relative flex flex-col gap-4">
       <TinyMCEEditor value={text} onEditorChange={handleChange} />
-      <Button primary={true} onClick={handleSave} className="w-max self-center">
-        Save
+      <Button
+        primary={true}
+        isDisabled={buttonState === "pending"}
+        success={buttonState === "fulfilled"}
+        danger={buttonState === "rejected"}
+        onClick={handleSave}
+        className="w-max self-center"
+      >
+        {buttonState === "pending"
+          ? "Saving..."
+          : buttonState === "fulfilled"
+          ? "Saved!"
+          : buttonState === "rejected"
+          ? "Retry"
+          : "Save"}
       </Button>
     </div>
   );
