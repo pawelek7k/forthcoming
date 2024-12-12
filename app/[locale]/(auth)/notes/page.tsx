e88777e5e6a32fdd5611c0289f803c4adb062a27
pawelek <k.pawelek@vp.pl>
2024-12-12 16:23:39 +0100

    Create page.tsx

---
+++ app/[locale]/(auth)/notes/page.tsx
@@ -0,0 +1,17 @@
+import { Section } from "@/app/components/Section";
+
+export const metadata = {
+  title: "Future - Write your ideas!",
+  description: "Future",
+};
+
+const NotesPage = () => {
+  // const t = useTranslations("headings");
+  return (
+    <Section>
+      <h1>hejka</h1>
+    </Section>
+  );
+};
+
+export default NotesPage;
