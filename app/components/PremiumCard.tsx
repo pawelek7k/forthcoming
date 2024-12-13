ae3ae346227b9e7c9310d79aa42491137bfab9ad
pawelek <k.pawelek@vp.pl>
2024-12-13 17:55:32 +0100

    Update PremiumCard.tsx

--- app/components/PremiumCard.tsx
+++ app/components/PremiumCard.tsx
@@ -1,3 +1,12 @@
+import { LinkComponent } from "./Link";
+
 export const PremiumCard = () => {
-  return <div>PremiumCArd</div>;
+  return (
+    <div>
+      <h2>Buy premium and save your gold thoughts!</h2>
+      <LinkComponent to="/" className="hover-effect">
+        Learn More
+      </LinkComponent>
+    </div>
+  );
 };
