47374ee2092b41ad4c7f7c798615c37bc2a887b4
pawelek <k.pawelek@vp.pl>
2024-12-13 17:48:16 +0100

    .

--- app/[locale]/(auth)/notes/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Section } from "@/app/components/Section";
-import { ChildrenType } from "@/types/children";
-
-export default async function RootLayout({ children }: ChildrenType) {
-  return <Section>{children} </Section>;
-}
