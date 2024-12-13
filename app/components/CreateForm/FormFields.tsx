ea9ee6a58308d9d608b6a7fe06fbce476e29623a
pawelek <k.pawelek@vp.pl>
2024-12-13 17:25:05 +0100

    Update FormFields.tsx

--- app/components/CreateForm/FormFields.tsx
+++ app/components/CreateForm/FormFields.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import { initialFormData } from ".";
 import { InputField } from "../InputField";
 
@@ -6,23 +7,26 @@ type FormFieldsProps = {
   handleChange: (key: keyof typeof initialFormData) => (value: string) => void;
 };
 
-export const FormFields = ({ formData, handleChange }: FormFieldsProps) => (
-  <div className="flex flex-col gap-4">
-    <InputField
-      id="title"
-      placeholder="Title"
-      label="Title"
-      value={formData.title}
-      onChange={(e) => handleChange("title")(e.target.value)}
-    />
-    <InputField
-      component="textarea"
-      id="description"
-      placeholder="Description"
-      label="Description"
-      className="mb-0"
-      value={formData.description}
-      onChange={(e) => handleChange("description")(e.target.value)}
-    />
-  </div>
-);
+export const FormFields = ({ formData, handleChange }: FormFieldsProps) => {
+  const t = useTranslations("global");
+  return (
+    <div className="flex flex-col gap-4">
+      <InputField
+        id="title"
+        placeholder={t("title")}
+        label={t("title")}
+        value={formData.title}
+        onChange={(e) => handleChange("title")(e.target.value)}
+      />
+      <InputField
+        component="textarea"
+        id="description"
+        placeholder={t("description")}
+        label={t("description")}
+        className="mb-0"
+        value={formData.description}
+        onChange={(e) => handleChange("description")(e.target.value)}
+      />
+    </div>
+  );
+};
