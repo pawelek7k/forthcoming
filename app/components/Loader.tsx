ee44e5fb9ac4ed1a11fceace8cf3ecb9da06d104
pawelek <k.pawelek@vp.pl>
2024-12-14 18:24:27 +0100

    Update Loader.tsx

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,5 +1,5 @@
 export const Loader = () => (
-  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-999 dark:bg-zinc-900/50">
+  <div className="fixed inset-0 flex items-center justify-center z-999 bg-zinc-950/90">
     <div className="flex flex-row gap-2">
       <div className="w-4 h-4 rounded-full bg-rose-950 animate-bounce z-999"></div>
       <div className="w-4 h-4 rounded-full bg-rose-900 animate-bounce shadow-lg shadow-rose-900 [animation-delay:-.3s] z-999"></div>
