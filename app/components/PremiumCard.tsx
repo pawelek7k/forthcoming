b361d2104d23f168352a033e2b32a6dbf66810eb
pawelek <k.pawelek@vp.pl>
2024-12-14 16:42:11 +0100

    Update PremiumCard.tsx

--- app/components/PremiumCard.tsx
+++ app/components/PremiumCard.tsx
@@ -2,7 +2,7 @@ import { LinkComponent } from "./Link";
 
 export const PremiumCard = () => {
   return (
-    <div className="border border-rose-950 w-max p-4 rounded-md">
+    <div className="border border-rose-950 w-max p-4 rounded-md bg-dark-primary-bg h-64 shadow-md">
       <h2>Buy premium and save your gold thoughts!</h2>
       <LinkComponent to="/" className="hover-effect">
         Learn More
