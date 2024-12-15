d9b77083e836ae1196644323a72ea6436c9cbb84
pawelek <k.pawelek@vp.pl>
2024-12-15 14:07:38 +0100

    add animation

--- app/[locale]/(auth)/notes/page.tsx
+++ app/[locale]/(auth)/notes/page.tsx
@@ -1,6 +1,7 @@
 import { ParticlesBackground } from "@/app/components/ParticlesBackground";
 import { PremiumCardModal } from "@/app/components/PremiumCard";
 import { Section } from "@/app/components/Section";
+import { TiLockClosed } from "react-icons/ti";
 
 export const metadata = {
   title: "Future - Write your ideas!",
@@ -11,6 +12,9 @@ const NotesPage = () => {
   return (
     <Section className="relative w-full h-screen">
       <ParticlesBackground />
+      <div className="bg-zinc-900 w-44 h-60 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center rounded-md shadow-lg">
+        <TiLockClosed className="w-12 h-12 shadow-lg animate-colorShift" />
+      </div>
       <PremiumCardModal />
     </Section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,15 @@ export default {
         "dark-primary-bg": " radial-gradient(70% 53% at 36% 76%, #4c0519 0%, #073AFF00 100%),radial-gradient(42% 53% at 15% 94%, #18181b 7%, #073AFF00 100%),radial-gradient(42% 53% at 34% 72%, #18181b 7%, #073AFF00 100%),radial-gradient(18% 28% at 35% 87%, #18181b 7%, #073AFF00 100%),radial-gradient(31% 43% at 7% 98%, #18181b 24%, #073AFF00 100%),radial-gradient(21% 37% at 72% 23%, #09090b 24%, #073AFF00 100%),radial-gradient(35% 56% at 91% 74%, #09090b 9%, #073AFF00 100%),radial-gradient(74% 86% at 67% 38%, #09090b 24%, #073AFF00 100%),linear-gradient(125deg, #09090b 1%, #09090b 100%);",
         "home-img": "linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.5)), url('/images/library.jpg')",
       },
+      keyframes: {
+        colorShift: {
+          '0%, 100%': { color: '#f43f5e' },
+          '50%': { color: '#3b82f6' },
+        },
+      },
+      animation: {
+        colorShift: 'colorShift 3s infinite',
+      },
     },
   },
   plugins: [],
