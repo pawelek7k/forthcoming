cac172f5b2e45583f9b2ecfae55b653b3cd38f4a
pawelek <k.pawelek@vp.pl>
2024-12-15 17:30:49 +0100

    Update page.tsx

--- app/[locale]/(auth)/home/page.tsx
+++ app/[locale]/(auth)/home/page.tsx
@@ -1,52 +1,12 @@
-"use client";
+import { HomeRenderContent } from "@/app/components/HomeRenderContent";
 
-import { Loader } from "@/app/components/Loader";
-import { RenderBooks } from "@/app/components/RenderBooks";
-import { Section } from "@/app/components/Section";
-import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
-import { fetchBooks } from "@/lib/redux/slices/booksSlice";
-import { useRouter } from "@/navigation";
-import { useSession } from "next-auth/react";
-import dynamic from "next/dynamic";
-import { useEffect } from "react";
-
-const Welcome = dynamic(() => import("@/app/components/Welcome"), {
-  ssr: false,
-  loading: () => <Loader />,
-});
+export const metadata = {
+  title: "Future - Home",
+  description: "Future",
+};
 
 const HomePage = () => {
-  const { data: session, status: sessionStatus } = useSession();
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const { books, status, error } = useAppSelector((state) => state.books);
-
-  useEffect(() => {
-    if (sessionStatus === "loading") return;
-    if (!session) {
-      router.push("/");
-    }
-  }, [session, sessionStatus, router]);
-
-  useEffect(() => {
-    if (status === "idle" && session) {
-      dispatch(fetchBooks());
-    }
-  }, [dispatch, status, session]);
-
-  if (sessionStatus === "loading") return <Loader />;
-
-  const email = session?.user?.email ?? "unknown@example.com";
-  const username = session?.user?.name ?? email.split("@")[0];
-
-  return (
-    <Section>
-      <Welcome email={email} username={username} />
-      {status === "loading" && <Loader />}
-      {status === "failed" && <p>{error}</p>}
-      {status === "succeeded" && <RenderBooks books={books} />}
-    </Section>
-  );
+  return <HomeRenderContent />;
 };
 
 export default HomePage;
