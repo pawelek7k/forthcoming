ccce3fd2bdafc77c6e0f0f99e6ea5e49a8888f62
pawelek <k.pawelek@vp.pl>
2024-12-17 12:37:33 +0100

    Update Paragraph.tsx

--- app/components/Paragraph.tsx
+++ app/components/Paragraph.tsx
@@ -1,13 +1,31 @@
 import type { ChildrenType } from "@/types/children";
 import classNames from "classnames";
+import { useTranslations } from "next-intl";
+import { useMemo } from "react";
 
 type ParagraphProps = {
-  children: ChildrenType;
+  children?: ChildrenType;
   className?: string;
+  namespace?: string;
 };
 
-export const Paragraph = ({ children, className }: ParagraphProps) => {
+export const Paragraph = ({
+  children,
+  className,
+  namespace,
+}: ParagraphProps) => {
+  const t = useTranslations();
+
   const paragraphClasses = classNames("text-sm sm:text-base", className);
 
-  return <p className={paragraphClasses}>{children}</p>;
+  const translatedMessage = useMemo(() => {
+    return namespace ? t(namespace) : "";
+  }, [namespace, t]);
+
+  return (
+    <p className={paragraphClasses}>
+      {" "}
+      {translatedMessage && `${translatedMessage} `} {children}
+    </p>
+  );
 };
