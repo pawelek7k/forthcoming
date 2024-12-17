34150c3a71ed0f638dce9d0f2ad3713d9b40187b
pawelek <k.pawelek@vp.pl>
2024-12-17 12:35:04 +0100

    Create Paragraph.tsx

---
+++ app/components/Paragraph.tsx
@@ -0,0 +1,13 @@
+import { ChildrenType } from "@/types/children";
+import classNames from "classnames";
+
+type ParagraphProps = {
+  children: ChildrenType;
+  className?: string;
+};
+
+export const Paragraph = ({ children, className }: ParagraphProps) => {
+  const paragraphClasses = classNames("text-sm sm:text-base", className);
+
+  return <p className={paragraphClasses}>{children}</p>;
+};
