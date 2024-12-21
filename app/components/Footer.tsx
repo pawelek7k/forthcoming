25eabff82cb862839d0707b14a3297649ac92549
pawelek <k.pawelek@vp.pl>
2024-12-21 15:26:13 +0100

    add footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,12 @@
+import { Paragraph } from "./Paragraph";
+
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="bg-gradient-to-t to-zinc-950 from-rose-950 p-4">
+      <Paragraph
+        namespace="footer.copyright"
+        className="text-center text-zinc-100"
+      ></Paragraph>
+    </footer>
+  );
 };
