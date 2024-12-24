95e440510d718c1c45a5f7429e0de0c977054075
pawelek <k.pawelek@vp.pl>
2024-12-24 14:22:38 +0100

    Update CardDetailsForm.tsx

--- app/components/Notes/CardDetailsForm.tsx
+++ app/components/Notes/CardDetailsForm.tsx
@@ -1,27 +1,27 @@
-"use client";
+type InputField = {
+  id: string;
+  placeholder: string;
+};
 
 export const CardDetailsForm = () => {
+  const inputFields: InputField[] = [
+    { id: "cardNumber", placeholder: "1234 5678 1234 5678" },
+    { id: "expiryDate", placeholder: "MM/YY" },
+    { id: "cvv", placeholder: "CVV" },
+  ];
+
   return (
     <form>
       <div className="flex gap-4 border border-gray-300 rounded-lg bg-zinc-50 z-10 overflow-hidden">
-        <input
-          type="text"
-          id="cardNumber"
-          className=" block w-full p-2 bg-zinc-50 outline-none z-0"
-          placeholder="1234 5678 1234 5678"
-        />
-        <input
-          type="text"
-          id="expiryDate"
-          className="block w-full p-2 bg-zinc-50 outline-none"
-          placeholder="MM/YY"
-        />
-        <input
-          type="text"
-          id="cvv"
-          className=" block w-full p-2 bg-zinc-50 outline-none"
-          placeholder="CVV"
-        />
+        {inputFields.map(({ id, placeholder }) => (
+          <input
+            key={id}
+            type="text"
+            id={id}
+            className="block w-full p-2 bg-zinc-50 outline-none placeholder:text-sm"
+            placeholder={placeholder}
+          />
+        ))}
       </div>
     </form>
   );
