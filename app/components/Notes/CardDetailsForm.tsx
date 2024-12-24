16056d0b46c9285a49a1003455c3739027ceae0d
pawelek <k.pawelek@vp.pl>
2024-12-24 14:20:24 +0100

    Update CardDetailsForm.tsx

--- app/components/Notes/CardDetailsForm.tsx
+++ app/components/Notes/CardDetailsForm.tsx
@@ -3,32 +3,25 @@
 export const CardDetailsForm = () => {
   return (
     <form>
-      <div className="flex gap-4">
-        <div className="flex-1">
-          <input
-            type="text"
-            id="cardNumber"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md outline-none"
-            placeholder="1234 5678 1234 5678"
-          />
-        </div>
-
-        <div className="flex-1">
-          <input
-            type="text"
-            id="expiryDate"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md outline-none"
-            placeholder="MM/YY"
-          />
-        </div>
-        <div className="flex-1">
-          <input
-            type="text"
-            id="cvv"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md outline-none"
-            placeholder="CVV"
-          />
-        </div>
+      <div className="flex gap-4 border border-gray-300 rounded-lg bg-zinc-50 z-10 overflow-hidden">
+        <input
+          type="text"
+          id="cardNumber"
+          className=" block w-full p-2 bg-zinc-50 outline-none z-0"
+          placeholder="1234 5678 1234 5678"
+        />
+        <input
+          type="text"
+          id="expiryDate"
+          className="block w-full p-2 bg-zinc-50 outline-none"
+          placeholder="MM/YY"
+        />
+        <input
+          type="text"
+          id="cvv"
+          className=" block w-full p-2 bg-zinc-50 outline-none"
+          placeholder="CVV"
+        />
       </div>
     </form>
   );
