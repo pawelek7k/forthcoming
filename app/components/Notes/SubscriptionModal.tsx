2277f47a8c5b3fc5d686091b853e33f33db3cf94
pawelek <k.pawelek@vp.pl>
2024-12-24 16:29:58 +0100

    Update SubscriptionModal.tsx

--- app/components/Notes/SubscriptionModal.tsx
+++ app/components/Notes/SubscriptionModal.tsx
@@ -26,12 +26,19 @@ export const SubscriptionModal = ({
         exit={{ scale: 0.8, opacity: 0 }}
         className="bg-zinc-100 rounded-2xl shadow-lg w-[500px] text-zinc-950 relative h-96"
       >
-        <div className=" bg-zinc-900 text-zinc-100 rounded-xl p-6 h-64 w-[500px]">
-          <Heading as="h2" namespace="premium.purchaseHeading" />
-          <Paragraph namespace="premium.purchaseParagraph" />
+        <div className=" bg-zinc-900 text-zinc-100 rounded-xl p-6 h-64 w-[500px] flex flex-col gap-2">
+          <Heading
+            as="h2"
+            namespace="premium.purchaseHeading"
+            className="text-xl"
+          />
+          <Paragraph
+            namespace="premium.purchaseParagraph"
+            className="text-xs text-zinc-400 text-lg"
+          />
           <ul className="flex items-center justify-center gap-4">
             {features.map((feature, index) => (
-              <li key={index} className="flex items-center gap-1">
+              <li key={index} className="flex items-center gap-1 text-sm">
                 <FcApproval />
                 {feature}
               </li>
