664e32aaef7884a1fe79ecf7df2196367fee7f0a
pawelek <k.pawelek@vp.pl>
2024-12-24 16:56:57 +0100

    Update SubscriptionFlow.tsx

--- app/components/Notes/SubscriptionFlow.tsx
+++ app/components/Notes/SubscriptionFlow.tsx
@@ -6,22 +6,26 @@ import Notiflix from "notiflix";
 
 export const SubscriptionFlow = () => {
   const [step, setStep] = useState<"info" | "purchase">("info");
+  const [isModalVisible, setIsModalVisible] = useState(true);
 
   const handleNextStep = () => setStep("purchase");
 
-  const handleClose = () => setStep("info");
+  const handleClose = () => {
+    setStep("info");
+    setIsModalVisible(false);
+  };
 
   const handleSubscribe = () => {
     Notiflix.Notify.success("Thank you for subscribing!");
-    handleClose();
+    setIsModalVisible(false);
   };
 
   return (
     <>
-      {step === "info" && (
+      {isModalVisible && step === "info" && (
         <PremiumCardModal onClose={handleClose} onProceed={handleNextStep} />
       )}
-      {step === "purchase" && (
+      {isModalVisible && step === "purchase" && (
         <SubscriptionModal
           onSubscribe={handleSubscribe}
           onClose={handleClose}
