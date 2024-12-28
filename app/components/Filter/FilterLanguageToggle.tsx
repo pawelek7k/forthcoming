cf43b082306dda2a969db9d39cbe92b172d45cd3
pawelek <k.pawelek@vp.pl>
2024-12-28 15:03:22 +0100

    Update FilterLanguageToggle.tsx

--- app/components/Filter/FilterLanguageToggle.tsx
+++ app/components/Filter/FilterLanguageToggle.tsx
@@ -1,8 +1,8 @@
-import { ToggleSwitch } from "../ToggleSwitch";
-import { useDispatch, useSelector } from "react-redux";
 import { setSelectedLanguage } from "@/lib/redux/slices/booksSlice";
 import { RootState } from "@/lib/redux/store";
 import { useTranslations } from "next-intl";
+import { useDispatch, useSelector } from "react-redux";
+import { ToggleSwitch } from "../ToggleSwitch";
 
 export const FilterLanguageToggle = () => {
   const dispatch = useDispatch();
@@ -13,11 +13,11 @@ export const FilterLanguageToggle = () => {
 
   return (
     <div className="flex gap-2 items-center justify-between">
-      <p className=" text-neutral-100 text-sm font-semibold uppercase">
+      <p className="text-xs md:text-sm text-zinc-100 font-semibold uppercase">
         {t("langSwitch")}
       </p>
       <div className="flex items-center gap-2">
-        <span className="text-sm">PL</span>
+        <span className="text-xs md:text-sm text-zinc-100">PL</span>
         <ToggleSwitch
           name="languageToggle"
           value={selectedLanguage || "eng"}
@@ -25,7 +25,7 @@ export const FilterLanguageToggle = () => {
           offValue="eng"
           onChange={(value) => dispatch(setSelectedLanguage(value))}
         />
-        <span className="text-sm">ENG</span>
+        <span className="text-xs md:text-sm text-zinc-100">ENG</span>
       </div>
     </div>
   );
